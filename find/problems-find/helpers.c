d7395fd5442866f43c79677c0836a7f98c110bbd
Israel Mesa <mesa.israel50@gmail.com>
2017-11-10 03:30:14 +0000

    in progress

--- find/problems-find/helpers.c
+++ find/problems-find/helpers.c
@@ -3,9 +3,9 @@
  *
  * Helper functions for Problem Set 3.
  */
- 
-#include <cs50.h>
 
+#include <cs50.h>
+#include <stdio.h>
 #include "helpers.h"
 
 /**
@@ -13,7 +13,20 @@
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
+    if (n < 0)
+    {
+        return false;
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (values[i] == value)
+            {
+                return true;
+            }
+        }
+    }
     return false;
 }
 
@@ -22,6 +35,32 @@ bool search(int value, int values[], int n)
  */
 void sort(int values[], int n)
 {
-    // TODO: implement a sorting algorithm
-    return;
+    for (int i = 0; i < (n - 2); i++)
+    {
+        if(values[i + 1] < values[i])
+        {
+            int temp = values[i];
+            values[i] = values[i + 1];
+            values[i + 1] = temp;
+        }
+    }
+    for(int j = 0; j < n; j++)
+    {
+        printf("%i\n", values[j]);
+    }
 }
+
+// var swaps = 0;
+//     for (var i = 0; i < n; i++) {
+//         for (var j = 0; j < n - 1; j++) {
+//             if (a[j] > a[j + 1]) {
+//                 var tmp = a[j];
+//                     a[j] = a[j + 1];
+//                     a[j + 1] = tmp;
+//                 swaps++;
+//             }
+//         }
+//             if (swaps == 0) {
+//                 break;
+//             }
+//     }
